b10fd091176b220d369b852689b032a16f03cc60
Bhuvan588 <bhuvan27112003@gmail.com>
2025-03-22 13:36:20 +0530

    Learnt spring security and custom properties

--- myapp/src/main/java/com/example/springboot/demo/myapp/rest/SimpleRestController.java
+++ myapp/src/main/java/com/example/springboot/demo/myapp/rest/SimpleRestController.java
@@ -1,10 +1,20 @@
 package com.example.springboot.demo.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleRestController {
+
+    //add properties for instructor.name and student.class
+
+    @Value("${instructor.name}")
+    private String instructorName;
+
+    @Value("${student.name}")
+    private String studentName;
+
 
     //expose "/" that returns My Application
 
@@ -17,5 +27,11 @@ public class SimpleRestController {
     public String getLearningGoals()
     {
         return "Learn 10 modules each day.";
+    }
+
+    @GetMapping("/classInfo")
+    public String classInfo()
+    {
+        return "Instructor: " + instructorName + "Student: " + studentName;
     }
 }
